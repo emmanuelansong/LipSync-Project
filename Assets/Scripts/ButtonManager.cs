a81a1f31804ff810278adf46df7d907849d0436e
Emmanuel Ansong <emmanuelkansong@gmail.com>
2022-02-18 15:21:23 +0000

    added another audio

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -10,12 +10,16 @@ public class ButtonManager : MonoBehaviour
     public AudioSource placeholderAudioSource;
     public LipSyncFromAudioFile lipsync;
     public TextAsset SSML;
+
+    Transform Panel;
     // Start is called before the first frame update
     void Start()
     {
         
         Text text = transform.GetChild(0).GetComponent<Text>();
         text.text = pb.name;
+
+        Panel = transform.parent;
 
     }
 
@@ -29,14 +33,17 @@ public class ButtonManager : MonoBehaviour
          else
              gameObject.GetComponent<Button>().interactable = true;*/
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (pb.time > 0)
         {
-            pb.time += 1;
-        }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                pb.time += 1;
+            }
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            pb.time -= 1;
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                pb.time -= 1;
+            }
         }
     }
     private void OnDrawGizmos()
@@ -45,12 +52,18 @@ public class ButtonManager : MonoBehaviour
         text.text = pb.name;
     }
 
+    //on click
     public void PlayTimeline()
     {
         if (lipsync.key != null && lipsync.region != null)
         {
             lipsync.FromFile_Viseme(placeholderAudioSource, SSML.text);
-            pb.Stop();
+
+            foreach(Transform x in Panel.transform)
+            {
+                x.GetComponent<ButtonManager>().pb.Stop();
+            }
+            
             pb.Play();
             
 
